a06e45998272ec9921685c3e5dd96edfdca35f4a
Kongduino <dbvic@me.com>
2024-02-18 23:57:20 +0800

    Better hexDump fn

--- Helper.h
+++ Helper.h
@@ -18,28 +18,36 @@ void stockUpRandom() {
 #endif // NEED_DEBUG
 }
 
-void hexDump(unsigned char *buf, uint16_t len) {
-  String s = "|", t = "| |";
-  Serial.println(F("  |.0 .1 .2 .3 .4 .5 .6 .7 .8 .9 .a .b .c .d .e .f |"));
-  Serial.println(F("  +------------------------------------------------+ +----------------+"));
-  for (uint16_t i = 0; i < len; i += 16) {
-    for (uint8_t j = 0; j < 16; j++) {
-      if (i + j >= len) {
-        s = s + "   "; t = t + " ";
-      } else {
-        char c = buf[i + j];
-        if (c < 16) s = s + "0";
-        s = s + String(c, HEX) + " ";
-        if (c < 32 || c > 127) t = t + ".";
-        else t = t + (String(c));
+void hexDump(uint8_t* buf, uint16_t len) {
+  // Something similar to the Unix/Linux hexdump -C command
+  // Pretty-prints the contents of a buffer, 16 bytes a row
+  char alphabet[17] = "0123456789abcdef";
+  uint16_t i, index;
+  Serial.print(F("   +------------------------------------------------+ +----------------+\n"));
+  Serial.print(F("   |.0 .1 .2 .3 .4 .5 .6 .7 .8 .9 .a .b .c .d .e .f | |      ASCII     |\n"));
+  for (i = 0; i < len; i += 16) {
+    if (i % 128 == 0) Serial.print(F("   +------------------------------------------------+ +----------------+\n"));
+    char s[] = "|                                                | |                |\n";
+    // pre-formated line. We will replace the spaces with text when appropriate.
+    uint8_t ix = 1, iy = 52, j;
+    for (j = 0; j < 16; j++) {
+      if (i + j < len) {
+        uint8_t c = buf[i + j];
+        // fastest way to convert a byte to its 2-digit hex equivalent
+        s[ix++] = alphabet[(c >> 4) & 0x0F];
+        s[ix++] = alphabet[c & 0x0F];
+        ix++;
+        if (c > 31 && c < 128) s[iy++] = c;
+        else s[iy++] = '.'; // display ASCII code 0x20-0x7F or a dot.
       }
     }
-    uint8_t index = i / 16;
+    index = i / 16;
+    // display line number then the text
+    if (i < 256) Serial.write(' ');
     Serial.print(index, HEX); Serial.write('.');
-    Serial.println(s + t + "|");
-    s = "|"; t = "| |";
+    Serial.print(s);
   }
-  Serial.println(F("  +------------------------------------------------+ +----------------+"));
+  Serial.print(F("   +------------------------------------------------+ +----------------+\n"));
 }
 
 void getRandomBytes(uint8_t *buff, uint8_t count) {
